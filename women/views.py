7a861f51106dbc65e48b520b30b64ad30141db6e
Burton <shopmage@yandex.ru>
2022-05-07 12:52:30 +0300

    Create new project

---
+++ women/views.py
@@ -0,0 +1,50 @@
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render, redirect
+from .models import *
+
+menu = [{'title': 'О сайте', 'url_name': 'about'},
+        {'title': 'Добавить статью', 'url_name': 'add_page'},
+        {'title': 'Обратная связь', 'url_name': 'contact'},
+        {'title': 'Войти', 'url_name': 'login'},
+        ]
+
+
+def index(request):
+    posts = Women.objects.all()
+    cats = Category.objects.all()
+
+    context = {'posts': posts,
+               'cats': cats,
+               'menu': menu,
+               'title': 'Главная страница',
+               'cat_selected': 0,
+               }
+    return render(request, 'women/index.html', context=context)
+
+
+def about(request):
+    return render(request, 'women/about.html', {'title': 'О сайте'})
+
+
+def addpage(request):
+    return HttpResponse(f'<h1>Добавить страницу</h1><p></p>')
+
+
+def contact(request):
+    return HttpResponse(f'<h1>Контакты</h1>')
+
+
+def login(request):
+    return HttpResponse(f'<h1>Авторизоваться</h1>')
+
+
+def show_post(request, post_id):
+    return HttpResponse(f'<h1>Отображение статьи с id {post_id}</h1>')
+
+
+def show_category(request, cat_id):
+    return HttpResponse(f'<h1>Отображение категории с id {cat_id}</h1>')
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
