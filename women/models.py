e56d0bba2974f3dcb6de93d9150163adcfad749c
Burton <shopmage@yandex.ru>
2022-05-08 14:10:19 +0300

    add new form

--- women/models.py
+++ women/models.py
@@ -4,12 +4,13 @@ from django.urls import reverse
 
 class Women(models.Model):
     title = models.CharField(max_length=255, verbose_name='Заголовок')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
     content = models.TextField(blank=True, verbose_name='Текст статьи')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
     is_published = models.BooleanField(default=True, verbose_name='Публикация')
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категории')
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категории')
 
     class Meta:
         verbose_name = 'Известные женщины'
@@ -20,11 +21,12 @@ class Women(models.Model):
         return self.title
 
     def get_absolut_url(self):
-        return reverse('post', kwargs={'post_id': self.pk})
+        return reverse('post', kwargs={'post_slug': self.slug})
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
 
     class Meta:
         verbose_name = 'Категории'
@@ -35,4 +37,4 @@ class Category(models.Model):
         return self.name
 
     def get_absolut_url(self):
-        return reverse('category', kwargs={'cat_id': self.pk})
+        return reverse('category', kwargs={'cat_slug': self.slug})
--- women/views.py
+++ women/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .forms import AddPostForm
 from .models import *
 
 menu = [{'title': 'О сайте', 'url_name': 'about'},
@@ -11,10 +13,8 @@ menu = [{'title': 'О сайте', 'url_name': 'about'},
 
 def index(request):
     posts = Women.objects.all()
-    cats = Category.objects.all()
 
     context = {'posts': posts,
-               'cats': cats,
                'menu': menu,
                'title': 'Главная страница',
                'cat_selected': 0,
@@ -27,7 +27,18 @@ def about(request):
 
 
 def addpage(request):
-    return HttpResponse(f'<h1>Добавить страницу</h1><p></p>')
+    if request.method == 'POST':
+        form = AddPostForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            try:
+                Women.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, 'Ошибка добавления поста')
+    else:
+        form = AddPostForm()
+    return render(request, 'women/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
 
 
 def contact(request):
@@ -38,21 +49,27 @@ def login(request):
     return HttpResponse(f'<h1>Авторизоваться</h1>')
 
 
-def show_post(request, post_id):
-    return HttpResponse(f'<h1>Отображение статьи с id {post_id}</h1>')
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+
+    context = {
+        'post': post,
+        'menu': menu,
+        'title': post.title,
+        'cat_selected': post.cat_id,
+    }
+    return render(request, 'women/post.html', context=context)
 
 
-def show_category(request, cat_id):
-    posts = Women.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
+def show_category(request, cat_slug):
+    posts = Women.objects.filter(cat__slug=cat_slug)
 
     if len(posts) == 0:
         raise Http404()
     context = {'posts': posts,
-               'cats': cats,
                'menu': menu,
                'title': 'Отображение по рубрикам',
-               'cat_selected': cat_id,
+               'cat_selected': cat_slug,
                }
     return render(request, 'women/index.html', context=context)
 
